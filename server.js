31cbf2659986ea6bc97423d77d4e01548de96711
Andrew Manteza <manteza14@gmail.com>
2022-01-12 15:41:19 +0800

    latest first assessment

--- server.js
+++ server.js
@@ -1,45 +1,76 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
 const app = express()
 
 
-app.use(bodyParser.urlencoded({ extended: true}))
-
-app.listen(3000, function() {
-    console.log('here on port 3000')
-
-})
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html')
-})
-
-app.post('/LTOform' ,(req, res) => {
-
-    console.log(req.body)
-}
-)
 
 
-// Connection URI
 const uri =
-  "mongodb+srv://andrewmanteza21:1UDejNyGIB0jWafB@cluster0.jre2j.mongodb.net/Driver_records?retryWrites=true&w=majority";
-
-// Create a new MongoClient
-const client = new MongoClient(uri);
-async function run() {
-  try {
-    // Connect the client to the server
-    await client.connect();
-
-    // Establish and verify connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Connected successfully to server");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+"mongodb+srv://andrewmanteza21:IzTwjAW2UUoKZZe0@cluster0.jre2j.mongodb.net/Driver_records?retryWrites=true&w=majority";{
+    
 }
-run().catch(console.dir);
+
+const client = new MongoClient(uri);
+client.connect();
+console.log("connected to mongodb database")
+
+  const database = client.db('Driver_records');
+  const userrecords = database.collection('user_records');
+
+  console.log('test1')
+
+
+      app.use(bodyParser.urlencoded({ extended: true}))
+
+      app.set('view engine', 'ejs')
+      //res.render(view,locals)
+
+      app.listen(3000, function() {
+        console.log('here on port 3000')
+    })
+      app.get('/', function (req, res) {
+      res.sendFile(__dirname + '/index.html')
+      console.log('this Get from index.html works')
+    })
+      app.post('/user_records', (req, res) => {
+      result = userrecords.insertOne(req.body)
+      .then(result => {
+      res.redirect('/')    
+      console.log("this Form sending works")  
+    
+    })
+      .catch(error => 
+      console.error(error))
+      console.log(req.body)
+    
+    })
+      console.log('Read Function Test')
+      app.get('/', (req,res) => {
+      const cursor = db.collection('user_records').find()
+      console.log(cursor)
+      console.log('Read Function Success')
+    })
+      
+    
+
+
+
+    
+    
+    
+        
+            
+    
+  
+    
+
+
+   
+
+
+
+
+
+
+    // Replace the uri string with your MongoDB deployment's connection string.
